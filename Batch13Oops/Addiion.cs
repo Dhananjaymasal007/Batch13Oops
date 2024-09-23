21581c27e7c91121eaf69532a6bde50d2080460f
Dhanaji <Dhanaji@DESKTOP-61VT7VD>
2024-09-23 09:25:58 +0530

    add a class

---
+++ Batch13Oops/Addiion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch13Oops
+{
+    internal class Addiion
+    {
+        static void Main()
+        {
+            int a = 10;
+            int b = 10;
+            int c;
+            c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
