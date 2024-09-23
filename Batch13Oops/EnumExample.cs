791d3fdaaa44037e4d4dd9375de2405e82b84205
rinkita <Rahul@LAPTOP-T6MOK2OP>
2024-09-23 08:03:18 +0530

    add

--- Batch13Oops/EnumExample.cs
+++ Batch13Oops/EnumExample.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Batch13Oops
+amespace Batch13Oops
 {
-    public enum Days
+    public enum Daysn
     {
         Sunday,
         Monday,
