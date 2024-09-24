e791013a70dcddce24e5f5dcd84668dc368bf848
Vishal <vishaljagannath547@gmail.com>
2024-09-24 21:52:08 +0530

    Added a new Demo Sub Branch.

---
+++ Batch13Oops/DemoAddtion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch13Oops
+{
+    internal class DemoAddtion
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public static void Main(string[] args)
+        {
+            DemoAddtion demoAddtion = new DemoAddtion();
+            demoAddtion.add(1, 2);
+        }
+    }
+}
