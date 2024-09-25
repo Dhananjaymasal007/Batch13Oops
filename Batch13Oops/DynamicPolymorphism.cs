6b0d010c39a9d3510386f3fadd8f0319c4daa5ec
Dhananjay Masal <dhananjay9096@gmail.com>
2024-09-25 08:29:23 +0530

    Polymorhism Implementation as on 25/09/24

---
+++ Batch13Oops/DynamicPolymorphism.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch13Oops
+{
+  
+        //Dynamic polymorphism/ method overriding / late binding 
+        //method name same  parameter also same but different class
+
+
+        //base class
+
+        class Shape
+        {
+            public virtual void Draw()
+            {
+                Console.WriteLine("Drawing a shape");
+            }
+        }
+        
+        //deriverd clas 1
+        class circle : Shape
+        {
+            //override draw method
+            public override void Draw() 
+                        {
+                Console.WriteLine("Driwing a circle");
+            }
+        }
+       
+        //derived class 2
+
+        class Rectangle : Shape
+
+        {
+            //override draw method
+            public override void Draw()
+            {
+                Console.WriteLine("Drawing Rectangle");
+            }
+        }
+
+    internal class DynamicPolymorphism
+    {
+        static void Main()
+        {
+            //creating object of different classess
+            
+            Shape shape1 = new Shape();
+            Shape shape2 = new circle();
+            Shape shape3 = new Rectangle();
+
+            //calling the draw method on each object
+
+            shape1.Draw();
+            shape2.Draw();
+            shape3.Draw();
+        }
+    }
+}
---
+++ Batch13Oops/StaticPolymorphism.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch13Oops
+{
+    class MathOperations
+    {
+        //Method overloading or static polymorphism or compile time polymorphism or Early binding
+        //method name same class also same but different parameteres
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+    }
+
+    internal class StaticPolymorphism
+    {
+        static void Main()
+        {
+            MathOperations math = new MathOperations();
+
+
+            /// calling the overlaod methods
+
+            int sum = math.Add(2, 4);
+            double sum1 = math.Add(3.5, 6);
+
+            Console.WriteLine("Sum of intergeres:"+ sum);
+            Console.WriteLine("Sum of doubles:"+ sum1);
+
+        }
+    }
+}
