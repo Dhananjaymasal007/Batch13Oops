6c8296f7c80e36f1d1d8a20286ddd460dcb814f5
Dhananjay Masal <dhananjay9096@gmail.com>
2024-10-02 08:32:17 +0530

    non Generic collection arraylist and Hasttable

---
+++ Batch13Oops/ArrayListTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch13Oops
+{
+    internal class ArrayListTest
+    {
+        static void Main()
+        {
+            // creating object of an array list
+
+            ArrayList lst = new ArrayList();
+
+            ///adding elements in to array list
+
+            lst.Add(1);
+            lst.Add("Hello");
+            lst.Add(false);
+
+            //access the elements/items
+
+            Console.WriteLine("Arraylist Elements:");
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item);
+            }
+
+            // lst.(1);
+            //lst.();
+           // lst.Remove(false);
+            Console.WriteLine("Reversed Arraylist Elements:");
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+
+    }
+}
---
+++ Batch13Oops/Hahtable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch13Oops
+{
+    internal class Hahtable
+    {
+       static void Main()
+       
+        {
+            Hashtable ht = new Hashtable();
+
+            //add key-value paring to the hashtable
+
+            ht.Add(101, "JAVA");
+            ht.Add(102, "DOTNET");
+            ht.Add(103, 9890);
+            ht.Add(104, true);
+
+
+           
+            //accesssing the elements although typecasting
+
+            string value1 = (string)ht[101];
+            string value2 = (string)ht[102];
+            string value3 = Convert.ToString((int)ht[103]);
+            bool value4= (bool)ht[104];
+
+            //display value
+            Console.WriteLine($"101:{value1}");
+
+            Console.WriteLine($"102:{value2}");
+
+            Console.WriteLine($"103:{value3}");
+
+            Console.WriteLine($"104:{value4}");
+
+            ht[103] = "AWS";
+
+
+        }
+    }
+}
